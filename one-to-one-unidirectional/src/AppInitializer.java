ed91d3e7ea334e1a9070887172c75902758ab5e2
Asantha <asanthadhananjaya@gmail.com>
2023-04-24 15:28:29 +0530

    completed the one to one unidirectional mapping

--- one-to-one-unidirectional/src/AppInitializer.java
+++ one-to-one-unidirectional/src/AppInitializer.java
@@ -1,4 +1,26 @@
+import entity.Laptop;
+import entity.Student;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.awt.*;
 
 public class AppInitializer {
+    public static void main(String[] args) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
 
+            Laptop laptop= new Laptop();
+            laptop.setBrand("lenovo");
+            session.save(laptop);
+
+            Student student =new Student() ;
+            student.setStudentName("Lakmal");
+            student.setLaptop(laptop);
+            session.save(student);
+
+            transaction.commit();
+
+        }
+    }
 }
--- one-to-one-unidirectional/src/entity/Laptop.java
+++ one-to-one-unidirectional/src/entity/Laptop.java
@@ -1,6 +1,37 @@
 package entity;
 
+import javax.persistence.*;
+
+@Entity(name = "laptop_table")
 public class Laptop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "lap_id")
     private long lapId;
     private String brand;
+
+    public Laptop(long lapId, String brand) {
+        this.lapId = lapId;
+        this.brand = brand;
+    }
+
+    public Laptop() {
+    }
+
+    public long getLapId() {
+        return lapId;
+    }
+
+    public void setLapId(long lapId) {
+        this.lapId = lapId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
 }
